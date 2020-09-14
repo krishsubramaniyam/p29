49f330ba61b6c188ee49195eada53c522fe9c31e
krishsubramaniyam <69412044+krishsubramaniyam@users.noreply.github.com>
2020-09-14 13:07:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var ground
+var block1,block2,block3,block4,block5,block7,block8,block9;
+var block10,block11,block12,block13,block14,block15,block16;
+
+
+function setup() {
+  createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+
+  ground = new Ground(600,height,1200,20);
+  
+  block1 = new Box();
+  
+}
+
+function draw() {
+  background(255,255,255);  
+
+  ground.display();
+  drawSprites();
+}
